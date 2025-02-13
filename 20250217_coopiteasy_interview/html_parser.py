6c3d950bf52719db7c7f951f3cec1808edc47bfc
Simon Hick <simon.hick.t@gmail.com>
2025-02-14 00:30:03 +0100

    graph done

--- 20250217_coopiteasy_interview/html_parser.py
+++ 20250217_coopiteasy_interview/html_parser.py
@@ -1,15 +1,21 @@
 import requests
 import validators
+import networkx as nx
+import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 
 def html_parser(target_url: str) -> None:
     target_domain_name = target_url.split('/')[2]
 
     urls_to_visit = [ target_url ]
-    urls_found = [ target_url ]
+    urls_to_node_dict = { target_url:0 }
+    node_count = 0
+    G = nx.Graph()
+    G.add_node(node_count)
 
     while urls_to_visit:
         current_url = urls_to_visit.pop()
+
         response = requests.get(current_url)
         parsed_html = BeautifulSoup(response.text, 'html5lib')
         output_file = "output/" + parsed_html.find('title').string
@@ -24,8 +30,16 @@ def html_parser(target_url: str) -> None:
             href_domain_name = href.split('/')[2]
             if href_domain_name != target_domain_name:
                 continue
-            elif href in urls_found:
-                continue
-
-            urls_found.append(href)
-            urls_to_visit.append(href)
+            if href not in urls_to_node_dict:
+                node_count += 1
+                G.add_node(node_count)
+                urls_to_node_dict[href] = node_count
+                urls_to_visit.append(href)
+            
+            G.add_edge(
+                urls_to_node_dict[current_url],
+                urls_to_node_dict[href]
+            )
+    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.savefig("output/graph.png")
+    
--- 20250217_coopiteasy_interview/main.py
+++ 20250217_coopiteasy_interview/main.py
@@ -1,13 +1,19 @@
 import time
+import os
+import shutil
 import validators
 from html_parser import html_parser
 
 def main():
+    if os.path.exists('output'):
+        shutil.rmtree('output')
+    os.makedirs('output')
+
     url = ""
     while not validators.url(url):
         url = input("Please enter the url from which the html files will be downloaded: \n")
         if not validators.url(url):
-            print("Invalid url.")
+            print("\nInvalid url.")
 
     html_parser(url)
 
